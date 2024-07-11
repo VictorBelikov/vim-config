348c129ba717a3f4dca70db0aaa1ee4c394456af
VictorBelikov <Viktar_Belikau@epam.com>
2024-07-11 11:30:24 +0300

    feat: add scrolloff

--- plugins/astrocore.lua
+++ plugins/astrocore.lua
@@ -26,6 +26,7 @@ return {
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
         showtabline = 0,
+        scrolloff = 7,
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
